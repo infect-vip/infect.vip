2489567aca6e54f00620b71a9848d254d1bfa70e
infect-vip <127279040+infect-vip@users.noreply.github.com>
2023-03-08 01:34:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,77 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
-        <title>isolate</title>
-        <meta name="description" content="isolation.world">
-        <meta itemprop="name" content="isolation">
-        <meta itemprop="description" content="isolation.world">
-        <meta itemprop="image" content="https://isolation.world/assets/media/images/embed.png">
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:title" content="isolation">
-        <meta name="twitter:description" content="isolation.world">
-        <meta name="twitter:image" content="https://isolation.world/assets/media/images/embed.png">
-	    <link rel="shortcut icon" type="image/png" href="/assets/media/images/bat.gif">
-	    <link rel="stylesheet" href="assets/css/stylev.css?ver=4">
-	    <script disable-devtool-auto src="assets/js/disable-devtool.min.js?ver=2"></script>
-	    
+        <title>infect.vip</title>
     </head>
     
     <body>
-		<div class="enter" onclick="proceed();">
-			<span class="center">click to proceed</span>
-		</div>
-		
-		<div id="main"> 
-		    <div class="center">
-                <pre class="spider">                               ;               ,                               
-                             ,;                 '.                             
-                            ;:                   :;                            
-                           ::                     ::                           
-                           ::                     ::                           
-                           ':                     :                            
-                            :.                    :                            
-                         ;' ::                   ::  '                         
-                        .'  ';                   ;'  '.                        
-                       ::    :;                 ;:    ::                       
-                       ;      :;.             ,;:     ::                       
-                       :;      :;:           ,;"      ::                       
-                       ::.      ':;  ..,.;  ;:'     ,.;:                       
-                        "'"...   '::,::::: ;:   .;.;""'                        
-                            '"""....;:::::;,;.;"""                             
-                        .:::.....'"':::::::'",...;::::;.                       
-                       ;:' '""'"";.,;:::::;.'""""""  ':;                       
-                      ::'         ;::;:::;::..         :;                      
-                     ::         ,;:::::::::::;:..       ::                     
-                     ;'     ,;;:;::::::::::::::;";..    ':.                    
-                    ::     ;:"  ::::::"""'::::::  ":     ::                    
-                     :.    ::   ::::::;  :::::::   :     ;                     
-                      ;    ::   :::::::  :::::::   :    ;                      
-                       '   ::   ::::::....:::::'  ,:   '                       
-                        '  ::    :::::::::::::"   ::                           
-                           ::     ':::::::::"'    ::                           
-                           ':       """""""'      ::                           
-                            ::                    ;:                            
-                            ':;    <a onclick="copyDiscord()" id="discord"></a>    ;:                            
-                              ';                ;'                              
-                                  <a href="" target="_blank"></a>                                  
-                                <a href="" target="_blank"></a>                                
-                                <a href="" target="_blank"></a>                                
-                                <a href="" target="_blank"></a>                                
-                                <div id="player">
-<input type="range" value="0.3" min="0" max="1" onchange="vol()" step="0.1" id="r">
-                </pre>
-		    </div>
-		</div>
-		
-		<audio loop preload="auto" id="audio">
-            <source src="VEA8IyhCTSUqQChUQDwjKEJNJSpAKFRAPCMoQk0lKkAoVEA8IyhCTSUqQChUQDwjKEJNJSpAKFRAPCMoQk0lKkAoVEA8IyhCTSUqQChUQDwjKEJNJSpAKFRAPCMoQk0lKkAoVEA8IyhCTSUqQCg=" type="audio/mp3" id="audio-source">
-        </audio>
-		
-		<video autoplay muted loop id="vid">
-					<source src="/assets/media/video/prt.mp4" type="video/mp4">
-		</video>
-		
-        <script src="assets/js/mainv.js?v=18"></script>
-			
-</html>
+hh
