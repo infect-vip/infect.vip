df395225ae0e33aabe6b69a5b5856bc70a5df5eb
infect-vip <127279040+infect-vip@users.noreply.github.com>
2023-03-08 01:28:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,116 +1,8 @@
-<?php
-
-$IP = (isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER['REMOTE_ADDR']);
-$Browser = $_SERVER['HTTP_USER_AGENT'];
-$Hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-$Webpage = $_SERVER['SCRIPT_NAME'];
-if (preg_match('/bot|Discord|robot|curl|spider|crawler|^$/i', $Browser)) {
-    exit();
-}
-
-date_default_timezone_set("America/New_York");
-$Date = date('d/m/Y');
-$Time = date('G:i:s');
-
-$Details = json_decode(file_get_contents("http://ip-api.com/json/{$IP}"));
-$VPNDet = json_decode(file_get_contents("https://proxycheck.io/v2/{$IP}?vpn=1&asn=1"));
-if ($VPNDet->proxy === "yes") {
-    $VPN = "true";
-} else {
-    $VPN = "false";
-}
-
-$Country = strtolower($Details->country);
-$CountryCode = strtolower($Details->countryCode);
-$Region = strtolower($Details->regionName);
-$City = strtolower($Details->city);
-$Zip = strtolower($Details->zip);
-$Lat = strtolower($Details->lat);
-$Lon = strtolower($Details->lon);
-$Org = strtolower($Details->org);
-$Webpage = $_SERVER['SCRIPT_NAME'];
-$Details->countryCode = strtolower($Details->countryCode);
-
-
-class Discord
-{
-    public function Visitor()
-    {
-        global $IP, $Browser, $Date, $Time, $VPN, $Country, $CountryCode, $Region, $City, $Zip, $Org, $Webpage,  $Lat, $Lon,  $Flag;
-
-        $Webhook = "https://discord.com/api/webhooks/1065004702061830326/T5vqqQkqKlXEXKC3BJeQKtTVId7va3wqBrw3a0HuMCwPJ28oRgg8QW4Fm97AiisOIY3o";
-
-        $InfoArr = array(
-            "embeds" => [array(
-
-                "title" => "visitor from $Country",
-                "color" => "2303786",
-
-                "fields" => [array(
-                    "name" => "ip",
-                    "value" => "$IP",
-                    "inline" => true
-                ),
-                    array(
-                        "name" => "vpn",
-                        "value" => "$VPN",
-                        "inline" => true
-                    ),
-                    array(
-                        "name" => "organization",
-                        "value" => "$Org",
-                        "inline" => true
-                    ),
-                    array(
-                        "name" => "geo lookup",
-                        "value" => "https://json.geoiplookup.io/$IP",
-                    ),
-                    array(
-                        "name" => "browser",
-                        "value" => "$Browser"
-                    ),
-                    array(
-                        "name" => "country/countrycode",
-                        "value" => "$Country/$CountryCode",
-                        "inline" => true
-                    ),
-                    array(
-                        "name" => "region | city | zip",
-                        "value" => "[$Region | $City | $Zip](https://www.google.com/maps/search/?api=1&query=$Lat,$Lon 'Google Maps Location (+/- 750M Radius)')",
-                        "inline" => true
-                    ),
-                    array(
-                        "name" => "webpage",
-                        "value" => "$Webpage",
-                        "inline" => true
-                    ),],
-
-                "footer" => array(
-                    "text" => "$Date $Time",
-                    "icon_url" => ""
-                ),
-            )],
-        );
-
-        $JSON = json_encode($InfoArr);
-
-        $Curl = curl_init($Webhook);
-        curl_setopt($Curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-        curl_setopt($Curl, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($Curl, CURLOPT_POSTFIELDS, $JSON);
-        curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
-		        return curl_exec($Curl);
-    }
-}
-$sendembed = New Discord();
-$sendembed->Visitor();
-?>
-
-
 <!DOCTYPE html>
 <html>
     <head>
         <base href="https://isolation.world">
+        
         <title>isolate</title>
         <meta name="description" content="isolation.world">
         <meta itemprop="name" content="isolation">
@@ -123,6 +15,64 @@ $sendembed->Visitor();
 	    <link rel="shortcut icon" type="image/png" href="/assets/media/images/bat.gif">
 	    <link rel="stylesheet" href="assets/css/stylev.css?ver=4">
 	    <script disable-devtool-auto src="assets/js/disable-devtool.min.js?ver=2"></script>
-	    <script language="Javascript" src="31017648d3a14eaa9003b900ff9ccc41.js"></script>
+	    
     </head>
+    
+    <body>
+		<div class="enter" onclick="proceed();">
+			<span class="center">click to proceed</span>
+		</div>
+		
+		<div id="main"> 
+		    <div class="center">
+                <pre class="spider">                               ;               ,                               
+                             ,;                 '.                             
+                            ;:                   :;                            
+                           ::                     ::                           
+                           ::                     ::                           
+                           ':                     :                            
+                            :.                    :                            
+                         ;' ::                   ::  '                         
+                        .'  ';                   ;'  '.                        
+                       ::    :;                 ;:    ::                       
+                       ;      :;.             ,;:     ::                       
+                       :;      :;:           ,;"      ::                       
+                       ::.      ':;  ..,.;  ;:'     ,.;:                       
+                        "'"...   '::,::::: ;:   .;.;""'                        
+                            '"""....;:::::;,;.;"""                             
+                        .:::.....'"':::::::'",...;::::;.                       
+                       ;:' '""'"";.,;:::::;.'""""""  ':;                       
+                      ::'         ;::;:::;::..         :;                      
+                     ::         ,;:::::::::::;:..       ::                     
+                     ;'     ,;;:;::::::::::::::;";..    ':.                    
+                    ::     ;:"  ::::::"""'::::::  ":     ::                    
+                     :.    ::   ::::::;  :::::::   :     ;                     
+                      ;    ::   :::::::  :::::::   :    ;                      
+                       '   ::   ::::::....:::::'  ,:   '                       
+                        '  ::    :::::::::::::"   ::                           
+                           ::     ':::::::::"'    ::                           
+                           ':       """""""'      ::                           
+                            ::                    ;:                            
+                            ':;    <a onclick="copyDiscord()" id="discord"></a>    ;:                            
+                              ';                ;'                              
+                                  <a href="" target="_blank"></a>                                  
+                                <a href="" target="_blank"></a>                                
+                                <a href="" target="_blank"></a>                                
+                                <a href="" target="_blank"></a>                                
+                                <div id="player">
+<input type="range" value="0.3" min="0" max="1" onchange="vol()" step="0.1" id="r">
+                </pre>
+		    </div>
+		</div>
+		
+		<audio loop preload="auto" id="audio">
+            <source src="VEA8IyhCTSUqQChUQDwjKEJNJSpAKFRAPCMoQk0lKkAoVEA8IyhCTSUqQChUQDwjKEJNJSpAKFRAPCMoQk0lKkAoVEA8IyhCTSUqQChUQDwjKEJNJSpAKFRAPCMoQk0lKkAoVEA8IyhCTSUqQCg=" type="audio/mp3" id="audio-source">
+        </audio>
+		
+		<video autoplay muted loop id="vid">
+					<source src="/assets/media/video/prt.mp4" type="video/mp4">
+		</video>
+		
+        <script src="assets/js/mainv.js?v=18"></script>
+			
 </html>
